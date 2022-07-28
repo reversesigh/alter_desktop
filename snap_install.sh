7339d6ada065071b1204f3fb830dd77832ad41c7
james <86951780+reversesigh@users.noreply.github.com>
2022-07-28 06:32:16 -0400

    Update snap_install.sh

--- snap_install.sh
+++ snap_install.sh
@@ -4,4 +4,3 @@ nativefier --name ALTER --platform linux --arch x64 --width 1634 --height 1355 -
 cp ALTER.desktop $HOME/.local/share/applications/
 cp alter.png ~/.icons/alter.png
 sudo cp -r ALTER-linux-x64 /usr/local/bin/
-rm -rf ALTER-linux-x64
