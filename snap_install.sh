b7a9f9f4062c7d7a8dae20edd433833db8c26357
james <86951780+reversesigh@users.noreply.github.com>
2022-07-28 06:23:43 -0400

    Update snap_install.sh

--- snap_install.sh
+++ snap_install.sh
@@ -1,10 +1,7 @@
-sudo snap install nativefier &&
-
-mkdir $HOME/NativeApps &&
-cd $HOME/NativeApps &&
-
-nativefier --name ALTER --platform linux --arch x64 --width 1634 --height 1355 --tray --disable-dev-tools --single-instance https://app.altermail.live/login &&
-
-cp ALTER.dekstop $HOME/.local/share/applications/ &&
-
-cp alter.png ~/.icons/alter.png &&
+#!/bin/bash
+sudo snap install nativefier
+nativefier --name ALTER --platform linux --arch x64 --width 1634 --height 1355 --tray --disabl>
+cp ALTER.desktop $HOME/.local/share/applications/
+cp $HOME/alter_files/alter_desktop/alter.png ~/.icons/alter.png
+sudo cp -r $HOME/alter_files/alter_desktop/ALTER-linux-x64 /usr/local/bin/
+rm -rf ALTER-linux-x64
