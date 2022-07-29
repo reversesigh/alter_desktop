18b9bffbe787e8d1ac86d8195ee463635422afa0
james <86951780+reversesigh@users.noreply.github.com>
2022-07-29 02:11:22 -0400

    Update snap_install-linux-x64.sh

--- snap_install-linux-x64.sh
+++ snap_install-linux-x64.sh
@@ -1,9 +1,31 @@
 #!/bin/sh
+echo
+echo "Installing nativefier via snap"
+echo
 sudo snap install nativefier
+echo
+echo "Creating ALTER_desktop"
+echo
 nativefier --name ALTER --platform linux --arch x64 --width 1634 --height 1355 --tray --disable-dev-tools --single-instance https://app.altermail.live/login
+echo
+echo "Copying ALTER.desktop to ~/.local/share/applications/"
+echo
 cp ALTER.desktop $HOME/.local/share/applications/
+echo
+echo "Copying alter.png to /usr/share/icons/"
+echo
 sudo cp alter.png /usr/share/icons/alter.png
-sudo cp -r ALTER-linux-x64 /usr/local/bin/
+echo
+echo "Creating ALTER_desktop directory"
+echo
+mkdir ~/ALTER_desktop
+echo
+echo "Copying ALTER_desktop to ~/ALTER_desktop"
+echo
+cp -r ALTER-linux-x64 ~/ALTER_desktop/
+echo
+echo "Removing ALTER-linux-x64 from GitHub directory"
+echo
 rm -rf ALTER-linux-x64
 echo
 echo "================================================================================================"
@@ -16,3 +38,4 @@ echo "You can also send me a message on ALTER @ az4jnb2ngz "
 echo
 echo "https://reversesigh.info - https://github.com/reversesigh"
 echo
+echo "ALTER_desktop is currently located at $HOME/ALTER_desktop. If you move be sure to edit ALTER.desktop accordingly"
