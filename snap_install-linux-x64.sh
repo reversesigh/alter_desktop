4a92f2daeb7b83a695301994d742914e8d93f32d
james <86951780+reversesigh@users.noreply.github.com>
2022-07-29 02:19:15 -0400

    Update snap_install-linux-x64.sh

--- snap_install-linux-x64.sh
+++ snap_install-linux-x64.sh
@@ -18,11 +18,11 @@ sudo cp alter.png /usr/share/icons/alter.png
 echo
 echo "Creating ALTER_desktop directory"
 echo
-mkdir ~/ALTER_desktop
+sudo mkdir /usr/bin/ALTER_desktop
 echo
-echo "Copying ALTER_desktop to ~/ALTER_desktop"
+echo "Copying ALTER_desktop to /usr/bin/ALTER_desktop"
 echo
-cp -r ALTER-linux-x64 ~/ALTER_desktop/
+sudo cp -r ALTER-linux-x64 /usr/bin/ALTER_desktop/
 echo
 echo "Removing ALTER-linux-x64 from GitHub directory"
 echo
@@ -38,4 +38,4 @@ echo "You can also send me a message on ALTER @ az4jnb2ngz "
 echo
 echo "https://reversesigh.info - https://github.com/reversesigh"
 echo
-echo "ALTER_desktop is currently located at $HOME/ALTER_desktop. If you move be sure to edit ALTER.desktop accordingly"
+echo "ALTER_desktop is currently located at /usr/bin/ALTER_desktop. If you move be sure to edit ALTER.desktop accordingly"
