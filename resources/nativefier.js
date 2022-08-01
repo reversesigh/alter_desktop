5152792fed06c2747d89bdc933ae7abd5d259ab6
james <86951780+reversesigh@users.noreply.github.com>
2022-08-01 04:34:41 -0400

    Create nativefier.js

---
+++ resources/nativefier.js
@@ -0,0 +1,44 @@
+var nativefier = require('nativefier').default;
+
+var options = {
+  name: 'ALTER_desktop',
+  targetUrl: 'https://app.altermail.live/login',
+  platform: 'linux',
+  arch: 'x64', 
+  version: '0.1.0',
+  overwrite: true,
+  asar: true,
+  icon: 'resources/alter.png',
+  inject: [ 'resources/scrollbar.css' ],
+  width: 1280,
+  height: 720,
+  verbose: true,
+  showMenuBar: false,
+  ignoreCertificate: false,
+  ignoreGpuBlacklist: false,
+  enableEs3Apis: false,
+  internalUrls: '.*altermaill.live.*',
+  blockExternalUrls: true,
+  insecure: false,
+  honest: false,
+  zoom: 1.004,
+  userAnget: 'chromium':
+  singleInstance: true,
+  clearCache: true,
+  tray: true,
+  disableDevTools: true,
+  fileDownloadOptions: {
+    saveAs: true,
+ },
+  webPreferences: { 
+    spellcheck: true,
+    },
+};
+
+nativefier(options, function (error, appPath) {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  console.log('App has been nativefied to', appPath);
+});
